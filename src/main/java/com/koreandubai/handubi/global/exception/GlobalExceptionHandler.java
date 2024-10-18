f06d9fca40f8741f78d8c42f0085e02589567113
sileeee <tldms201@naver.com>
2024-10-18 22:48:57 +0400

    feat: Add GlobalExceptionHandler

---
+++ src/main/java/com/koreandubai/handubi/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.koreandubai.handubi.global.exception;
+
+import com.koreandubai.handubi.global.common.FailResponse;
+import com.koreandubai.handubi.global.common.StatusEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public FailResponse handleIllegalArgumentException(IllegalArgumentException e){
+        log.error("IllegalArgumentException", e);
+        return FailResponse.builder()
+                .status(StatusEnum.BAD_REQUEST)
+                .errorMessage(e.getMessage())
+                .build();
+    }
+}
