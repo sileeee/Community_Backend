644f0c4ac2ff517ddcf1b4bd2b3bf909fdbdc83e
sileeee <tldms201@naver.com>
2025-01-30 21:48:10 +0400

    feat: retrieve top liked posts

--- src/main/java/com/koreandubai/handubi/repository/PostRepository.java
+++ src/main/java/com/koreandubai/handubi/repository/PostRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -25,4 +27,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "ORDER BY created_at DESC",
             nativeQuery = true)
     Page<Post> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
+
+    @Query("SELECT p.id FROM Post p WHERE p.category = :categoryType")
+    List<Long> findPostIdsByCategory(@Param("categoryType") CategoryType categoryType);
 }
